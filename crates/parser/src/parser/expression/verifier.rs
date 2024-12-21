f717b66e247d62cfd5e28746a0335dd8d036ad7a
Campbell <campbell.wass@gmail.com>
2024-12-22 12:43:03 +1300

    Remove visitor from parser

--- crates/parser/src/parser/expression/verifier.rs
+++ crates/parser/src/parser/expression/verifier.rs
@@ -1,39 +1,13 @@
 use super::*;
-use ecma_visit::{Visit, VisitWith};
-use global_common::Span;
 
 impl<I: Tokens> Parser<'_, I> {
     pub(in crate::parser) fn verify_expr(&mut self, expr: &Expr) {
-        let mut v = Verifier {
-            errors: vec![],
-            parser: self,
-        };
-
-        v.visit_expr(expr);
-
-        for (span, error) in v.errors {
-            self.emit_err(span, error);
-        }
-    }
-}
-
-struct Verifier<'a, I: Tokens> {
-    pub errors: Vec<(Span, SyntaxError)>,
-    parser: &'a Parser<'a, I>,
-}
-
-impl<I: Tokens> Visit<'_> for Verifier<'_, I> {
-    fn visit_assign_prop(&mut self, p: &AssignProp) {
-        self.errors.push((
-            get_span!(self.parser, p.node_id),
-            SyntaxError::AssignProperty,
-        ));
-    }
-
-    fn visit_expr(&mut self, e: &Expr) {
-        match *e {
-            Expr::Fn(..) | Expr::Arrow(..) => {}
-            _ => e.visit_children_with(self),
+        if let Expr::Object(e) = expr {
+            for prop in &e.props {
+                if let Prop::Assign(prop) = prop {
+                    self.emit_err(get_span!(self, prop.node_id), SyntaxError::AssignProperty);
+                }
+            }
         }
     }
 }
