373f61c5891efef291f1e0f55419142cc583cf8c
Campbell <campbell.wass@gmail.com>
2026-07-31 17:33:49 +1000

    Don't generate reserved keywords as variables names

--- crates/compiler/src/DefaultNameGenerator.rs
+++ crates/compiler/src/DefaultNameGenerator.rs
@@ -1,6 +1,7 @@
 use atoms::JsWord;
 use rustc_hash::FxHashSet;
 
+// TODO: why can't we use '_' as a first char?
 // It is important that the ordering of FIRST_CHAR is as close to NONFIRST_CHAR
 // as possible. Using the ASCII ordering is not a good idea. The reason
 // is that we cannot use numbers as FIRST_CHAR yet the ACSII value of numbers
@@ -47,15 +48,87 @@ impl DefaultNameGenerator {
 
             self.name_count += 1;
 
+            if is_reserved(&name) {
+                continue;
+            }
+
             let name = JsWord::from(name);
 
-            // Make sure it's not a JS keyword or reserved name.
-            // if !(TokenStream.isKeyword(name) || self.reservedNames.contains(name) || isBadName(name)) {
             if !self.reserved_names.contains(&name) {
                 return name;
             }
         }
     }
+}
+
+const RESERVED_STRINGS: [&str; 48] = [
+    "break",
+    "case",
+    "catch",
+    "class",
+    "const",
+    "continue",
+    "debugger",
+    "default",
+    "delete",
+    "do",
+    "else",
+    "enum",
+    "export",
+    "extends",
+    "false",
+    "finally",
+    "for",
+    "function",
+    "if",
+    "import",
+    "in",
+    "instanceof",
+    "new",
+    "null",
+    "package",
+    "return",
+    "super",
+    "switch",
+    "this",
+    "throw",
+    "true",
+    "try",
+    "typeof",
+    "var",
+    "void",
+    "while",
+    "with",
+    "implements",
+    "interface",
+    "let",
+    "package",
+    "private",
+    "protected",
+    "public",
+    "static",
+    "yield",
+    "eval",
+    "arguments",
+];
+
+const fn min_len(arr: &[&str; 48]) -> usize {
+    let mut min = usize::MAX;
+    let mut i = 0;
+    while i < arr.len() {
+        let len = arr[i].len();
+        if len < min {
+            min = len;
+        }
+        i += 1;
+    }
+    min
+}
+
+const MIN_RESERVED_LEN: usize = min_len(&RESERVED_STRINGS);
+
+fn is_reserved(name: &str) -> bool {
+    name.len() >= MIN_RESERVED_LEN && RESERVED_STRINGS.contains(&name)
 }
 
 // TODO: tests from closure
@@ -67,4 +140,19 @@ fn test_collision_with_past_names() {
         .map(|_| generator.generate_next_name())
         .collect::<FxHashSet<_>>();
     assert_eq!(names.len(), 1_000_000);
+}
+
+#[test]
+fn test_no_reserved_keywords() {
+    let mut generator = DefaultNameGenerator::default();
+    let names = (0..1_000_000)
+        .map(|_| generator.generate_next_name())
+        .collect::<FxHashSet<_>>();
+
+    for keyword in RESERVED_STRINGS {
+        assert!(
+            !names.contains(&JsWord::from(keyword)),
+            "Generated reserved word: '{keyword}'"
+        );
+    }
 }
