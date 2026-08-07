efa89ec385482e8370b0914ca5e8ff5d05365546
Campbell <campbell.wass@gmail.com>
2026-08-07 18:19:20 +1000

    Fix outdated macro derive name

--- crates/node_id/src/lib.rs
+++ crates/node_id/src/lib.rs
@@ -5,7 +5,7 @@ use quote::ToTokens;
 use syn::{self, *};
 
 #[proc_macro_derive(GetNodeIdMacro)]
-pub fn derive_string_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input: DeriveInput =
         syn::parse::<DeriveInput>(input).expect("failed to parse derive input");
     let mut tts = TokenStream::new();
