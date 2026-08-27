30635923a3df5083c5d9a4a29de16f4c7f657282
Campbell <campbell.wass@gmail.com>
2026-08-27 12:34:50 +1000

    Highlight active playground tab

--- crates/wasm/www/tabs.js
+++ crates/wasm/www/tabs.js
@@ -4,18 +4,29 @@ export class TabGroup {
   constructor(args) {
     this.#tabs = args.tabs;
 
-    for (const tab of args.tabs) {
+    for (let i = 0; i < args.tabs.length; i++) {
+      const tab = args.tabs[i];
+
       const button = document.createElement("button");
       button.textContent = tab.label;
+
+      if (i == 0) {
+        button.classList.add("active");
+      }
+
       button.addEventListener("click", () => {
+        button.classList.add("active");
         for (const otherTab of args.tabs) {
           if (otherTab !== tab) {
+            otherTab.button.classList.remove("active");
             otherTab.contentContainer.style.display = "none";
           }
         }
         tab.contentContainer.style.display = "initial";
       });
+
       args.buttonContainer.appendChild(button);
+      tab.button = button;
     }
   }
 
